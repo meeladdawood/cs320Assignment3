22abe6bbbdd86e2344388b696061cfaa48938542
Meelad <mdawood2@sdsu.edu>
2017-06-27 16:07:52 -0700

    changed the header

---
+++ prog3_3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+extern "C"{
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+
+int main(int argc, char* argv[]){
+	printf("Assignment #3-3, Meelad Dawood, mdawood2@sdsu.edu\n");
+	string filename = argv[1];
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+	luaL_dofile(L, filename.c_str());
+}
+
