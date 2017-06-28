07cb4d71951ba8c726e545085dbeea5600304f73
Meelad <mdawood2@sdsu.edu>
2017-06-27 17:23:24 -0700

    changed it to the top of the stack

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]){
 	lua_State *L = luaL_newstate();
 	luaL_openlibs(L);
 	luaL_dofile(L, filename.c_str());
-	char expression = *luaL_checkstring(L,1);
+	char expression = *luaL_checkstring(L,-1);
 	cout << expression << endl;
 
 }
