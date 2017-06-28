707d32c3dfd0e09dc2a76a180d100fff473a33e8
Meelad <mdawood2@sdsu.edu>
2017-06-27 17:53:38 -0700

    fixed the 2 digit input and space issues

--- prog3_2.lua
+++ prog3_2.lua
@@ -14,22 +14,17 @@ postfixStr = ""
         operands[index] = token
         index = index +1
       end
-
-     for i = 1, #operands
-       do
-          i = i + 1
-      end
    
     for i = 1, #operands
         do
           if tonumber(operands[i]) ~= nil 
                then
-                   postfixStr = postfixStr .. operands[i] 
+                   postfixStr = postfixStr .. operands[i] .. " " 
            else
               while(( next(operators) ~= nil) and
               (prec[tostring(operators[#operators])] >= prec[tostring((operands[i]))])  )
                     do
-                      postfixStr = postfixStr .. operators[#operators]
+                      postfixStr = postfixStr .. operators[#operators] .. " "
                       table.remove(operators)
                     end
            table.insert(operators,(operands[i]))
@@ -47,10 +42,4 @@ end
 s = io.read("*l")
 io.write(" ")
 res =InfixToPostfix(s)
-for i = 1, #res do
-    local temp = res:sub(i,i)
-    io.write(temp)
-    if i ~= #res then
-    	io.write(" ")
-    end
-end
+io.write(res)
